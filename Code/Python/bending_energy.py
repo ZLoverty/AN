c118602cf600f9a67f624a84466f8e1bbd5b5c94
Zhengyang Liu <liuzy19911112@gmail.com>
2025-01-20 23:07:44 -0500

    compute bending energy code

--- Code/Python/bending_energy.py
+++ Code/Python/bending_energy.py
@@ -2,17 +2,29 @@
 bending_energy.py
 =================
 Compute bending energy from a director field.
+1. Read angle field from a .npy file;
+2. Convert the angle field to a director field;
+3. Compute bending energy.
 
 Syntax
 ------
-python bending_energy.py directorDir
+python bending_energy.py directorDir maskDir
 
 directorDir: the director field, [M x N x 2].
+maskDir: the mask field, [M x N].
 
 Edit
 ----
-Jan 19, 2023 -- Initial commit.
+Jan 19, 2025 -- Initial commit.
+Jan 20, 2025 -- Fix the computation of director field, by requiring positive y component. Add mask processing.
 """
+
+import numpy as np
+import pandas as pd
+import os
+import argparse
+from myimagelib.myImageLib import show_progress
+from skimage import io
 
 def compute_gradient(field):
     """
@@ -24,8 +36,8 @@ def compute_gradient(field):
     Returns:
     gradient -- the gradient of the field, [M x N x 2 x 2].
     """
-    grad_x = np.gradient(field[::step, ::step, 0])
-    grad_y = np.gradient(field[::step, ::step, 1])
+    grad_x = np.gradient(field[:, :, 0])
+    grad_y = np.gradient(field[:, :, 1])
     grad = np.array([grad_x, grad_y]).transpose(1, 2, 0, 3)
     div = grad_x[0] + grad_y[1]
     curl = grad_y[0] - grad_x[1]
@@ -60,27 +72,39 @@ def angle_to_director(angle_8_bit):
     angle = angle_8_bit * np.pi / 255 - np.pi / 2
     director = np.array([np.cos(angle), np.sin(angle)]).transpose(1, 2, 0)
 
+    # require the y component director to be positive, otherwise flip the director
+    director[director[:,:,1]<=0] *= -1
+
     return director
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compute bending energy from a director field.")
-    parser.add_argument("directorDir", help="the director field, [M x N x 2].")
+    parser.add_argument("directorDir", help="the director field file dir.")
+    parser.add_argument("maskDir", help="the mask field file dir.")
     args = parser.parse_args()
+
+    # load mask
+    mask = io.imread(args.maskDir)
 
     # process the filename
     folder, filename = os.path.split(args.directorDir)
     name, ext = os.path.splitext(filename)
-    mainfolder = os.path.split(imgfolder)[0]
+    mainfolder = os.path.split(folder)[0]
     savefolder = os.path.join(mainfolder, "bending_energy")
+    if not os.path.exists(savefolder):
+        os.makedirs(savefolder)
 
     # compute bending energy
     angles = np.load(args.directorDir)
-    directors = angle_to_director(angles)
     energy_list = []
-    for director in directors:
+    nFrame = angles.shape[0]
+    for num, angle in enumerate(angles):
+        show_progress(num/nFrame, label=name)
+        director = angle_to_director(angle)
+        director[~mask] = np.nan
         bending_energy = compute_bending_energy(director)
-        energy_list.append(bending_energy)
+        energy_list.append(np.nanmean(bending_energy))
     
     # save bending energy data
     df = pd.DataFrame({"frame": range(len(energy_list)), "bending_energy": energy_list})
--- Code/Python/run.py
+++ Code/Python/run.py
@@ -1,8 +1,10 @@
+import pandas as pd
 import os
 from myimagelib.myImageLib import readdata
 
-folder = r"C:\Users\liuzy\Documents\AN\08 jun 2023\crop_channel"
-l = readdata(folder, "tif")
-
+folder = r"C:\Users\liuzy\Documents\AN\08 jun 2023"
+l = readdata(os.path.join(folder, "director_field"), "npy")
 for num, i in l.iterrows():
-    os.system("python director_field.py \"{}\"".format(i.Dir))
+    n, c = i.Name.split("_")
+    maskDir = os.path.join(folder, "mask", f"{c}{n}.tif")
+    os.system("python bending_energy.py \"{0}\" \"{1}\"".format(i.Dir, maskDir))
