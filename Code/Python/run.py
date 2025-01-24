4dcba06bd50fc1d8eec3297ca22aba42963ec520
Zhengyang Liu <liuzy19911112@gmail.com>
2025-01-24 00:32:23 -0500

    updated run script to have energy computation

--- Code/Python/run.py
+++ Code/Python/run.py
@@ -2,11 +2,10 @@ import pandas as pd
 import os
 from myimagelib.myImageLib import readdata
 
-folder = r"C:\Users\liuzy\Documents\AN\08 jun 2023"
+folder = r"G:\My Drive\NASync\Sample 1\nd2"
 l = readdata(os.path.join(folder, "qTensor"), "npy")
 for num, i in l.iterrows():
-    if "08" in i.Name:
-        # os.system("python qTensor.py \"{}\"".format(i.Dir))
-        n, c = i.Name.split("_")
-        maskDir = os.path.join(folder, "mask", f"{c}{n}.tif")
-        os.system("python bending_energy.py \"{0}\" \"{1}\"".format(i.Dir, maskDir))
+    # os.system("python qTensor.py \"{}\"".format(i.Dir))
+    n, c = i.Name.split("_")
+    maskDir = os.path.join(folder, "mask", f"{c}{n}.tif")
+    os.system("python bending_energy.py \"{0}\" \"{1}\"".format(i.Dir, maskDir))
